e3ba4e2fa32d60ec2dedf91baa34242529e24cee
Mohid Kazi <mohidkazix@gmail.com>
2024-12-09 15:43:58 +0530

    feat(nvim): Added my lazyvim neovim configuration

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  --{ "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      --colorscheme = "gruvbox",
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
